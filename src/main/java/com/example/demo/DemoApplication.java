468c2070065982703977c068c4b0f5afda71521f
liujava <983081882@qq.com>
2018-06-20 22:20:24 +0800

    boot下使用springMVC

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +20,8 @@ public class DemoApplication {
 	}
 
 
+	@GetMapping("/getMapping")
+	public String getMapping() {
+        return "getMapping";
+    }
 }
